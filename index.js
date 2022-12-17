3720375fed28af20875022efccc40fb3a6726bc2
Joshua Taylor <toasterrage@gmail.com>
2022-12-17 00:29:04 -0700

    Added modules

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // *****Load Modules*****
-
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 
 // *****DOM Variables and Buttons*****
