b8219331f4afde86fc538dff9eac64bb617fe2ad
Joshua Taylor <toasterrage@gmail.com>
2022-12-18 17:28:45 -0700

    Updated manager tests

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,28 @@
-// Manager Class JS
+const Manager = require('../lib/Manager');
+
+// Testing the Manager class
+describe('Manager class', () => {
+    // Test the getName() method
+    describe('getName method', () => {
+        it('returns manager name', () => {
+            const manager = new Manager('John', 42, 'john@gmail.com', '214');
+            expect(manager.getName()).toBe('John');
+        });
+    });
+
+    // Test the getEmail() method
+    describe('getEmail method', () => {
+        it('returns manager email', () => {
+            const manager = new Manager('John', 42, 'john@gmail.com', '214');
+            expect(manager.getEmail()).toBe('john@gmail.com');
+        });
+    });
+
+    // Test the getRole() method
+    describe('getRole method',() => {
+        it('returns manager role', () => {
+            const manager = new Manager('John', 42, 'john@gmail.com', '214');
+            expect(manager.getRole()).toBe('Manager');
+        });
+    });
+});
