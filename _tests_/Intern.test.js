3b54206ee10d011006a8fc3efdd83374c71140aa
Joshua Taylor <toasterrage@gmail.com>
2022-12-18 17:40:44 -0700

    Updated intern tests

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,36 @@
-// Intern Class JS
+const Intern = require('../lib/Intern');
+
+// Testing the Intern class
+describe('Intern class', () => {
+    // Test the getName() method
+    describe('getName method', () => {
+        it('returns intern name', () => {
+            const intern = new Intern('John', 42, 'john@gmail.com', 'Denver University');
+            expect(intern.getName()).toBe('John');
+        });
+    });
+
+    // Test the getEmail() method
+    describe('getEmail method', () => {
+        it('returns intern email', () => {
+            const intern = new Intern('John', 42, 'john@gmail.com', 'Denver University');
+            expect(intern.getEmail()).toBe('john@gmail.com');
+        });
+    });
+
+    // Test the getSchool() method
+    describe('getSchool method', () => {
+        it('returns intern school', () => {
+            const intern = new Intern('John', 42, 'john@gmail.com', 'Denver University');
+            expect(intern.getSchool()).toBe('Denver University');
+        });
+    });
+
+    // Test the getRole() method
+    describe('getRole method',() => {
+        it('returns intern role', () => {
+            const intern = new Intern('John', 42, 'john@gmail.com', 'Denver University');
+            expect(intern.getRole()).toBe('Intern');
+        });
+    });
+});
