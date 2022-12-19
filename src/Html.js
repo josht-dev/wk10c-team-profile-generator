69aaae7bf4c7a8c187596c9ea8ac7b47985be732
Joshua Taylor <toasterrage@gmail.com>
2022-12-19 15:36:58 -0700

    Added font family to generated html

--- src/Html.js
+++ src/Html.js
@@ -113,6 +113,8 @@ class Html {
                 <meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <!-- link for font, licensed under the SIL Open Font License (OFL) -->
+                <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
                 <link rel="stylesheet" href="./reset.css">
                 <link rel="stylesheet" href="./style.css">
                 <title>Team Profile Generator</title>
