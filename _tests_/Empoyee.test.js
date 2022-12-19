971edd76c1fadf3e291d64954ba679feda8ca884
Joshua Taylor <toasterrage@gmail.com>
2022-12-18 17:20:12 -0700

    Updated employee tests

--- _tests_/Empoyee.test.js
+++ _tests_/Empoyee.test.js
@@ -1,1 +1,28 @@
-// Employee Class JS
+const Employee = require('../lib/Employee');
+
+// Testing the Employee class
+describe('Employee class', () => {
+    // Test the getName() method
+    describe('getName method', () => {
+        it('returns employee name', () => {
+            const employee = new Employee('John', 42, 'john@gmail.com');
+            expect(employee.getName()).toBe('John');
+        });
+    });
+
+    // Test the getEmail() method
+    describe('getEmail method', () => {
+        it('returns employee email', () => {
+            const employee = new Employee('John', 42, 'john@gmail.com');
+            expect(employee.getEmail()).toBe('john@gmail.com');
+        });
+    });
+
+    // Test the getRole() method
+    describe('getRole method',() => {
+        it('returns employee role', () => {
+            const employee = new Employee('John', 42, 'john@gmail.com');
+            expect(employee.getRole()).toBe('Employee');
+        });
+    });
+});
