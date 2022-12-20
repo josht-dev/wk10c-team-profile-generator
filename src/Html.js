b3417b1a3d1bee4935ec2f6dd06130f2bbee51a3
Joshua Taylor <toasterrage@gmail.com>
2022-12-19 17:27:21 -0700

    Made tweeks to html/css generated

--- src/Html.js
+++ src/Html.js
@@ -52,42 +52,42 @@ class Html {
             };
 
             return `
-                <!-- Team member card container -->
-                <article class="container-card">
-                    <!-- Top name and title of card -->
-                    <div class="card-title">
-                        <!-- Team member name -->
-                        <div>
-                            <h2>${memberObj.name}</h2>
-                        </div>
-                        <!-- Team member title -->
-                        <div>
-                            <!-- Title name -->
-                            <h2>${memberObj.getRole()}</h2>
-                        </div>
+        <!-- Team member card container -->
+        <article class="container-card">
+            <!-- Top name and title of card -->
+            <div class="card-title">
+                <!-- Team member name -->
+                <div>
+                    <h2>${memberObj.name}</h2>
+                </div>
+                <!-- Team member title -->
+                <div>
+                    <!-- Title name -->
+                    <h2>${memberObj.getRole()}</h2>
+                </div>
+            </div>
+            <!-- Card content container-->
+            <div class="card-body">
+                <!-- Team member info container -->
+                <div>
+                    <!-- Team member ID -->
+                    <div class="card-field">
+                        <h3>ID: </h3>
+                        <span>${memberObj.id}</span>
                     </div>
-                    <!-- Card content container-->
-                    <div class="card-body">
-                        <!-- Team member info container -->
-                        <div>
-                            <!-- Team member ID -->
-                            <div class="card-field">
-                                <h3>ID: </h3>
-                                <span>${memberObj.id}</span>
-                            </div>
-                            <!-- Team member email -->
-                            <div class="card-field">
-                                <h3>Email: </h3>
-                                <a href="">${memberObj.email}</a>
-                            </div>
-                            <!-- Team member GitHub or office number -->
-                            <div class="card-field">
-                                <h3>${htmlH3(memberObj.getRole())}</h3>
-                                ${htmlUniqueTag(memberObj.getRole())}
-                            </div>
-                        </div>
+                    <!-- Team member email -->
+                    <div class="card-field">
+                        <h3>Email: </h3>
+                        <a href="">${memberObj.email}</a>
                     </div>
-                </article>
+                    <!-- Team member GitHub or office number -->
+                    <div class="card-field">
+                        <h3>${htmlH3(memberObj.getRole())}</h3>
+                        ${htmlUniqueTag(memberObj.getRole())}
+                    </div>
+                </div>
+            </div>
+        </article>
             `;
         }
 
@@ -98,41 +98,39 @@ class Html {
 
         // Loop through the engineers and interns arrays and create html cards
         this.engineers.forEach(val => {
-            engineerCards = engineerCards.concat(`\n${htmlCard(val)}`);
+            engineerCards = engineerCards.concat(`${htmlCard(val)}`);
         });
         this.interns.forEach(val => {   
-            internCards = internCards.concat(`\n${htmlCard(val)}`);
+            internCards = internCards.concat(`${htmlCard(val)}`);
         });
 
         return `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <!-- link for font, licensed under the SIL Open Font License (OFL) -->
-                <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-                <link rel="stylesheet" href="./reset.css">
-                <link rel="stylesheet" href="./style.css">
-                <title>Team Profile Generator</title>
-            </head>
-            <body>
-                <!-- Top title and nav bar -->
-                <header>
-                    <h1>The Team</h1>
-                </header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link for font, licensed under the SIL Open Font License (OFL) -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="./reset.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <!-- Top title and nav bar -->
+    <header>
+        <h1>The Team</h1>
+    </header>
 
-                <!-- Main content of page -->
-                <main>
-
-                    ${managerCard}
-                    ${engineerCards}
-                    ${internCards}
-                    
-                </main>
-            </body>
-            </html>  
+    <!-- Main content of page -->
+    <main>
+        ${managerCard}
+        ${engineerCards}
+        ${internCards}
+    </main>
+</body>
+</html>  
         `;
     }
 }
